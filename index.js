d472aab5b8d1797c8198409a950cb8f68b7c0d63
anmabrar <17101013@uap-bd.edu>
2023-04-04 03:14:28 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -67,11 +69,23 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email : email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn:'1h'});
+                return res.send({accessToken: token});
+            }
+            console.log(user);
+            res.status(404).send({accessToken : ' '})
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
-        })
+        });
     }
     finally {
 
