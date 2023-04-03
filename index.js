e9101414077aab94cc2569ec3ecd1ef7784071c8
anmabrar <17101013@uap-bd.edu>
2023-04-03 07:05:25 +0600

    get bookings

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
 
         })
 
+        app.get('/bookings', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email : email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
 
