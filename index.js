2cef18f798290c523889747c7faf2ee2746b25e2
anmabrar <17101013@uap-bd.edu>
2023-04-07 03:07:36 +0600

    CORS

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+const corsOptions = {
+    origin: "http://localhost:3000"
+  };
 
 //middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
+ 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bselz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
