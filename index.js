b2d1cf1b4cb2d5a315ef4457f0865a3bea995d11
anmabrar <17101013@uap-bd.edu>
2023-04-07 03:22:38 +0600

    CORS 03

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-const corsOptions = {
-    origin: "http://localhost:3000"
-  };
-
 //middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
  
 
